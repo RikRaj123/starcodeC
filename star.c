7e561fec0fc55a3e0d918207281c04e2b87e6284
RikRaj123 <92203867+RikRaj123@users.noreply.github.com>
2022-01-04 13:19:12 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    clrscr();
+    int i,j,n,k=0;
+    printf("enter the max no. of star in a row :- ");
+    scanf("%d ",&n);
+    for(i=0;i<(n+1);i++){
+        for (j=0;j<i ; j++)
+        {
+            k=k+1;
+            printf(" %d ",k);
+        }
+        printf("\n");
+    }
+}
